faaa380e6bf53949d682330e16a5ff44c0dc4990
Marcel Wolf <marcelwolf@me.com>
2026-02-22 10:10:44 +0100

    Initial Commit

---
+++ MeshHessen/MeshHessenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshHessenApp.swift
+//  MeshHessen
+//
+//  Created by Marcel Wolf on 22.02.26.
+//
+
+import SwiftUI
+
+@main
+struct MeshHessenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
